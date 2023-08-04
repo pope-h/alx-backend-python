018dc821cb45a20cd4675154207032d898408152
Ekarika Nsemeke <hamatthan@gmail.com>
2023-08-04 01:09:13 +0100

    correction to task 3, 5 & 6

--- 0x00-python_variable_annotations/5-sum_list.py
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
 """Defines type-annotated function sum_list"""
 
 from typing import List
--- 0x00-python_variable_annotations/6-sum_mixed_list.py
+++ 0x00-python_variable_annotations/6-sum_mixed_list.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
 """Defines a type-annotated function"""
 
 from typing import List, Union
