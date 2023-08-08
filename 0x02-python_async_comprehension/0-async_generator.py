ff3265f970e5c9ec0b36535b19fb473d1ca58fcd
Ekarika Nsemeke <hamatthan@gmail.com>
2023-08-08 19:43:31 +0100

    async comprehension task

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -7,6 +7,6 @@ import random
 
 async def async_generator():
     """Yields a random int 10 times"""
-    for _ in range(10):
+    for i in range(10):
         await asyncio.sleep(1)
         yield random.uniform(0, 10)
